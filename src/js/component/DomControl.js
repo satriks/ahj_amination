2e6ee8f4c97904844fa789caacc0a9d39093497e
Yury Vereskun <satriks@mail.ru>
2023-10-30 01:42:26 +0300

    done likes

--- src/js/component/DomControl.js
+++ src/js/component/DomControl.js
@@ -1,10 +1,12 @@
 import Collapser from "./collapser/Collapser";
 import ChatControl from "./chat/ChatControl";
+import LikeControl from "./likes/LikeControl";
 
 export default class DomControl{
     constructor(){
 
         this.collapse = new Collapser(document.querySelector('.collapser'));
+        this.like = new LikeControl(document.querySelector('.likes'));
         this.chat = null
     }
     initChat(){
