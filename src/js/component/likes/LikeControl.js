d236a0c4c921cb1926c4580b85289c481e14ab62
Yury Vereskun <satriks@mail.ru>
2023-10-30 01:43:06 +0300

    done likes

---
+++ src/js/component/likes/LikeControl.js
@@ -0,0 +1,32 @@
+export default class LikeControl{
+    constructor(element){
+        this.element = element;
+        this.likeBtn = null
+        this.create();
+    
+    }
+    create(){
+        const likeBtn = document.createElement('button');
+        likeBtn.className = 'like__Btn';
+        likeBtn.textContent = 'Like';    
+    
+        this.likeBtn = likeBtn;
+        likeBtn.addEventListener('click', this.createHeart);
+
+        this.element.appendChild(likeBtn);
+    }
+
+    createHeart = (event) => {
+        const num = Math.trunc(Math.random() * 4) + 1;
+
+        const heart = document.createElement('img');
+        heart.classList.add(`heart`, `heart-${num}`);
+        heart.style.left = `${this.likeBtn.offsetWidth / 2}px`
+
+        heart.addEventListener('animationend', () => {
+            heart.remove();
+        })
+        
+        this.likeBtn.insertAdjacentElement('beforebegin', heart)
+    }
+}
